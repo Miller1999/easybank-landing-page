6d54428324b5120f3de2e2a921820c8a7cd489da
Miller Arias <millerjavierarias@gmail.com>
2024-05-27 23:57:35 -0500

    Feat:Presentation Section

--- src/UIElements.ts
+++ src/UIElements.ts
@@ -1,4 +1,5 @@
 import Logo from "./assets/logo.svg";
+import Mockups from "./assets/image-mockups.png";
 
 const createElement = (
 	tagName: string,
@@ -20,6 +21,29 @@ const createButton = (className: string, html: string): HTMLElement => {
 	const button = createElement("button", className, html);
 	return button;
 };
+
+const createSection = (
+	className: string,
+	elements: HTMLElement[]
+): HTMLElement => {
+	const section = createElement("section", className);
+	if (elements.length !== 0) {
+		elements.forEach((element) => section.append(element));
+	}
+	return section;
+};
+
+const createDiv = (
+	className: string,
+	elements: HTMLElement[] = []
+): HTMLElement => {
+	const div = createElement("div", className);
+	if (elements.length !== 0) {
+		elements.forEach((element) => div.append(element));
+	}
+	return div;
+};
+
 const createHeader = (): HTMLElement => {
 	const header = createElement("header", "header");
 	const headerImg = createElement("img", "header__image", "", { src: Logo });
@@ -31,4 +55,40 @@ const createHeader = (): HTMLElement => {
 	return header;
 };
 
-export { createHeader };
+const createMain = (): HTMLElement => {
+	const main = createElement("main", "main");
+	const imagePresentation = createElement("img", "presentation__image", "", {
+		src: Mockups,
+	});
+	const backgroundPresentation = createDiv("presentation__background", [
+		imagePresentation,
+	]);
+	const presentationTitle = createElement(
+		"h1",
+		"presentation__info-title",
+		"Next generation digital banking"
+	);
+	const presentationDescription = createElement(
+		"p",
+		"presentation__info-description",
+		"Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more."
+	);
+	const presentationButton = createButton(
+		"presentation__button",
+		"Request Invite"
+	);
+	const presentationInfo = createDiv("presentation__info-container", [
+		presentationTitle,
+		presentationDescription,
+		presentationButton,
+	]);
+
+	const presentation = createSection("presentation", [
+		backgroundPresentation,
+		presentationInfo,
+	]);
+	main.append(presentation);
+	return main;
+};
+
+export { createHeader, createMain };
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import "normalize.css";
 import "./style.sass";
-import { createHeader } from "./UIElements";
+import { createHeader, createMain } from "./UIElements";
 
 const app = document.querySelector("#app");
 
 const header = createHeader();
+const main = createMain();
 
-app?.append(header);
+app?.append(header, main);
