f733a280ce13ef33be986e4a8dcd40141c02aa71
Miller Arias <millerjavierarias@gmail.com>
2024-05-28 01:10:04 -0500

    Feat:Features section

--- src/UIElements.ts
+++ src/UIElements.ts
@@ -1,5 +1,36 @@
 import Logo from "./assets/logo.svg";
 import Mockups from "./assets/image-mockups.png";
+import api from "./assets/icon-api.svg";
+import budgeting from "./assets/icon-budgeting.svg";
+import onboarding from "./assets/icon-onboarding.svg";
+import online from "./assets/icon-online.svg";
+
+const featuresItems = [
+	{
+		img: online,
+		title: "Online Banking",
+		description:
+			"Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.",
+	},
+	{
+		img: budgeting,
+		title: "Simple Budgeting",
+		description:
+			"See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.",
+	},
+	{
+		img: onboarding,
+		title: "Fast Onboarding",
+		description:
+			"We don’t do branches. Open your account in minutes online and start taking control of your finances right away.",
+	},
+	{
+		img: api,
+		title: "Open API",
+		description:
+			"Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.",
+	},
+];
 
 const createElement = (
 	tagName: string,
@@ -42,6 +73,19 @@ const createDiv = (
 		elements.forEach((element) => div.append(element));
 	}
 	return div;
+};
+
+const createCard = (
+	icon: string,
+	title: string,
+	description: string
+): HTMLElement => {
+	const card = createElement("article", "card");
+	const cardIcon = createElement("img", "card__icon", "", { src: icon });
+	const cardTitle = createElement("h3", "card__title", title);
+	const cardDescription = createElement("p", "card__description", description);
+	card.append(cardIcon, cardTitle, cardDescription);
+	return card;
 };
 
 const createHeader = (): HTMLElement => {
@@ -87,7 +131,29 @@ const createMain = (): HTMLElement => {
 		backgroundPresentation,
 		presentationInfo,
 	]);
-	main.append(presentation);
+	const featuresTitle = createElement(
+		"h2",
+		"features__info-title",
+		"Why choose Easybank?"
+	);
+	const featuresDescription = createElement(
+		"p",
+		"features__info-description",
+		"We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never before."
+	);
+	const featuresInfo = createDiv("features__info-container", [
+		featuresTitle,
+		featuresDescription,
+	]);
+	const featuresElements: HTMLElement[] = [];
+	featuresItems.forEach((feature) => {
+		const card = createCard(feature.img, feature.title, feature.description);
+		featuresElements.push(card);
+	});
+
+	const featuresContainer = createDiv("features__container", featuresElements);
+	const features = createSection("features", [featuresInfo, featuresContainer]);
+	main.append(presentation, features);
 	return main;
 };
 
